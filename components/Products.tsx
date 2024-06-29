13db12475401d7ff406a84658a71a952017d9736
Pronothurah <onsongojosphat28@gmail.com>
2024-06-29 13:32:06 +0300

    Tailwid css set up

--- components/Products.tsx
+++ components/Products.tsx
@@ -1,7 +1,7 @@
 // *********************
 // Role of the component: Showing products on the shop page with applied filter and sort
 // Name of the component: Products.tsx
-// Developer: Aleksandar Kuzmanovic
+// Developer:  Onsongo Onditi
 // Version: 1.0
 // Component call: <Products slug={slug} />
 // Input parameters: { slug }: any
